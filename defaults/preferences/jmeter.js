9a3157a4e46be616875f4f877d25366ab28caa4a
Richard Friedman <richardfriedman@yahoo.com>
2016-05-25 08:52:11 -0400

    Fixed regex

--- defaults/preferences/jmeter.js
+++ defaults/preferences/jmeter.js
@@ -1,5 +1,5 @@
 pref("extensions.selenium-ide.jmeter.includem",'redline13.com');
-pref("extensions.selenium-ide.jmeter.excludem",'(?i).*\.(bmp|css|js|gif|ico|jpe?g|png|swf|woff)[\?;].*');
+pref("extensions.selenium-ide.jmeter.excludem",'^[^?]*(\.gif|\.jpg|\.png|\.ico|\.css|\.js)(\?.*|$)');
 pref("extensions.selenium-ide.jmeter.maxrequests",100);
 pref("extensions.selenium-ide.jmeter.visualizer",true);
 pref("extensions.selenium-ide.jmeter.format",'jmeter');
